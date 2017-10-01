084fb4e6b3695ece96c0d0a2a7da6215c23c1724
Mathieu GOULIN <mathieu.Goulin@gadz.org>
2017-10-01 23:45:25 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,24 @@
 make mrproper
+[[ $? != "0" ]] && exit 1
+
 wget https://downloads.sourceforge.net/project/sdl-draw/SDL_draw/1.2.13/SDL_draw-1.2.13.tar.gz
+[[ $? != "0" ]] && exit 1
+
 tar xvf SDL_draw-1.2.13.tar.gz 
+[[ $? != "0" ]] && exit 1
+
 cd SDL_draw-1.2.13
+[[ $? != "0" ]] && exit 1
+
 ./configure && make
+[[ $? != "0" ]] && exit 1
+
 cp src/.libs/libSDL_draw.a ../lib/
+[[ $? != "0" ]] && exit 1
+
 cd ..
+[[ $? != "0" ]] && exit 1
+
 make
+[[ $? != "0" ]] && exit 1
+
